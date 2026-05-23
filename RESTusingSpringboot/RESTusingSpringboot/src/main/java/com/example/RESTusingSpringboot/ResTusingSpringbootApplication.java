1ea5d4897f8e08bb124e20d3be3f3c23a96835da
nitinshas3 <shastrin25@gmail.com>
2026-05-23 18:07:35 +0530

    initial commit

---
+++ RESTusingSpringboot/RESTusingSpringboot/src/main/java/com/example/RESTusingSpringboot/ResTusingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.RESTusingSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTusingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTusingSpringbootApplication.class, args);
+	}
+
+}
---
+++ RESTusingSpringboot/RESTusingSpringboot/src/test/java/com/example/RESTusingSpringboot/ResTusingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RESTusingSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTusingSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
